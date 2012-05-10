92d16c31a44b1f834bed1baae50ad4eb347b16af
Oscar Escalada <oscar@webanddesignstudio.com>
2012-05-10 03:49:25 -0700

    commit of vanilla rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PerfWeb::Application.config.secret_token = 'b9427f48736b0381e3608339204ac07b9362bf54dd46c796f1ab6a6b3c3e5397c2c3e843519623892214ea6ecf517d91ecc2d5b3121ff1fa59d1c9be69fa02ca'
